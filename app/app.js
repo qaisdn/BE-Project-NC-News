b9af0c1ccee8db6ec9db00feb92beab1655f474a
Qais Deen <qaisdeen@hotmail.com>
2024-10-14 21:27:19 +0100

    task 3 completed, added get /api

--- app/app.js
+++ app/app.js
@@ -3,10 +3,13 @@ const app = express();
 
 const { notfoundErrors } = require("../errors");
 const { getAllTopics } = require("../controllers/topics-controllers");
+const { getAPIinfo } = require("../controllers/endpoint-controllers");
 
 app.use(express.json());
 
+app.get("/api/", getAPIinfo);
 app.get("/api/topics", getAllTopics);
+
 app.use(notfoundErrors);
 
 module.exports = app;
