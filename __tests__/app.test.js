23f6498e25a0b120baf06e93232dbffb9cde6724
Qais Deen <qaisdeen@hotmail.com>
2024-10-15 09:29:53 +0100

    task 4 completed, added get articles id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -49,4 +49,33 @@ describe("NC News API testing", () => {
       });
     });
   });
+  describe("/api/articles/:article_id", () => {
+    test("return correct article when passed an article id ", () => {
+      return request(app)
+        .get("/api/articles/1")
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.articles).toEqual({
+            article_id: 1,
+            title: "Living in the shadow of a great man",
+            topic: "mitch",
+            author: "butter_bridge",
+            body: "I find this existence challenging",
+            created_at: "2020-07-09T20:11:00.000Z",
+            votes: 100,
+            article_img_url:
+              "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          });
+          expect(Object.keys(body.articles)).toHaveLength(8);
+        });
+    });
+    test("for articles with invalid ids we should expect a 404 error", () => {
+      return request(app)
+        .get("/api/articles/4311462")
+        .expect(404)
+        .then(({ body }) => {
+          expect(body.msg).toBe("article not found");
+        });
+    });
+  });
 });
--- app/app.js
+++ app/app.js
@@ -4,12 +4,28 @@ const app = express();
 const { notfoundErrors } = require("../errors");
 const { getAllTopics } = require("../controllers/topics-controllers");
 const { getAPIinfo } = require("../controllers/endpoint-controllers");
+const { getAllArticlesByID } = require("../controllers/article-controllers");
 
 app.use(express.json());
 
 app.get("/api/", getAPIinfo);
 app.get("/api/topics", getAllTopics);
+app.get("/api/articles/:article_id", getAllArticlesByID);
 
-app.use(notfoundErrors);
+app.use((error, request, response, next) => {
+  if (error.status) {
+    response.status(error.status).send({ msg: error.msg });
+  } else {
+    next(error);
+  }
+});
+
+app.use((request, response, next) => {
+  response.status(404).send({ msg: "URL not found" });
+});
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.status(500).send({ msg: "Internal Server Error" });
+});
 
 module.exports = app;
