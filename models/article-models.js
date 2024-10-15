5d3be59855fc7416b74d3bfc87ad67766a7e8ad3
Qais Deen <qaisdeen@hotmail.com>
2024-10-15 16:12:00 +0100

    added api/articles functionality (task 5)

--- models/article-models.js
+++ models/article-models.js
@@ -9,3 +9,15 @@ exports.fetchArticlesByID = (article_id) => {
     return rows[0];
   });
 };
+
+exports.fetchArticles = () => {
+  return db
+    .query(
+      `SELECT articles.* , COUNT(comments.article_id) AS comment_count FROM articles 
+        LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.created_at DESC
+        `
+    )
+    .then(({ rows: articles }) => {
+      return articles;
+    });
+};
